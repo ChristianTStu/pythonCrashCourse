6308ea1aa651bd3a3334a2c5baa4e1b4f848f1d6
Christian Stuart <christiantstu@gmail.com>
2024-09-06 15:04:04 -0500

    Completing buffet python script

---
+++ python_work/chapterFour/buffet.py
@@ -0,0 +1,13 @@
+fiveBasicFoods = ('rasberry', 'apple', 'banana', 'orange', 'tomato')
+
+print('Here are the basic foods the resturant provides:')
+
+# fiveBasicFoods[0] = 'mango'
+
+for foods in fiveBasicFoods:
+    print(foods)
+
+fiveBasicFoods = ('mango', 'apple', 'banana', 'orange', 'strawberry')
+
+for foods in fiveBasicFoods:
+    print(foods)
