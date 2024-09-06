d29ef41ff1615eab7a427ddc0f6afa221393135d
Christian Stuart <christiantstu@gmail.com>
2024-09-06 14:14:21 -0500

    Completing slices python script

---
+++ python_work/chapterFour/slices.py
@@ -0,0 +1,5 @@
+word_list = ["apple", "banana", "cherry", "date", "elderberry"]
+
+print(f'The first three items in my list are:{word_list[0:3]}')
+print(f'The middle three items in my list are:{word_list[1:4]}')
+print(f'The last three items in my list are:{word_list[-3:]}')
