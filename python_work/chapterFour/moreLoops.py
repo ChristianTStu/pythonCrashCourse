236699877cc44c55f6b478e35f3549c5e545ad0e
Christian Stuart <christiantstu@gmail.com>
2024-09-06 14:36:40 -0500

    Completing more loops python script

---
+++ python_work/chapterFour/moreLoops.py
@@ -0,0 +1,10 @@
+myFavoriteFoods = ['pizza', 'falafel', 'carrot cake', 'cannoli']
+
+myFriendsFavoriteFood = ['pizza', 'falafel', 'carrot cake', 'ice cream']
+
+for foods in myFavoriteFoods:
+    print(foods)
+
+for foods in myFriendsFavoriteFood:
+    print(foods)
+
