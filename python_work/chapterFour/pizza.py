23eda0dfb050ae9299f361dddf36bfb10b3aa157
Christian Stuart <christiantstu@gmail.com>
2024-09-05 22:28:33 -0500

    Completing pizza python script

---
+++ python_work/chapterFour/pizza.py
@@ -0,0 +1,6 @@
+pizza_list = ['cheese', 'pepperoni', 'veggie']
+
+for pizza in pizza_list:
+    print(f'I really want a {pizza.title()} pizza right now!')
+
+print('I like pizza so much, that if the world were made of pizza, I would be a happy man. There are other kinds of pizza I like as well. I like Chicken pizza and Basil pizza.')
